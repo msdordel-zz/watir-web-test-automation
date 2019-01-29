0536d93eface3fac3ece6792f0ecc026ad2878cc
Mark Dordel <dordelms@gmail.com>
2019-01-29 14:41:43 -0600

    Update ignore-template.rb

--- file/ignore-template.rb
+++ file/ignore-template.rb
@@ -3,54 +3,13 @@
 #DO NOT ever commit site URLs, usernames, or passwords to this file.
 
 #$BASE_URL = ENV['ART_URL']||'' 
-#$BASE_URL = ENV['ART_URL']||''
-$BASE_URL = ENV['ART_URL']||''
-#$BASE_URL = ENV['ART_URL']||''
 
 $ACCOUNT = ''
 
 #Live
-$NURSE = ''
-$PATIENT = ''
-$PPW = ''
-
-#QA
-$ADMIN = ''
-$QA = ''
-$PW = '' 
-$PATIENTQA = ''
-
-#Showcase
-$SADMIN = ''
-$SNURSE = ''
-$SPATIENT = ''
-$SPPW = ''
-
-#Global
-$GADMIN = ''
-$GNURSE = ''
-$GPATIENT2 = ''
-$GPATIENT1 = ''
-$GPPW = ''
-$GAPW = ''
-
-#edge
-$PATEDGE = ''
-
-
-$SUMMARY = ''
-$MB = ''
-$AT = ''
-
-#$SNFPROD = ''
-$SNFPROD = ''
-$SNFQA = ''
-
-$EMAIL = ''
-$MPW = ''
-
-$BDORG = ''
-$PORG = ''
+$admin
+$user1
+$user2
 
 #TODO
-#HL7 Creds maybe
+#More Creds
